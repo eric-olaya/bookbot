88025109339b6f41aee46b852882ff1ff81222bd
ericolaya <ericolaya@gmail.com>
2025-08-02 22:09:04 -0400

    read file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+def main():
+    contents = get_book_text("./books/frankenstein.txt")
+    print(contents)
+
+main()
