b907ac98a6cde587d7da589177c0ca495954fe4d
ericolaya <ericolaya@gmail.com>
2025-08-02 22:54:59 -0400

    print report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import count_words
 from stats import count_characters
+from stats import sort
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,7 +8,21 @@ def get_book_text(filepath):
 
 def main():
     contents = get_book_text("./books/frankenstein.txt")
-    print(f"{count_words(contents)} words found in the document")
-    print(count_characters(contents))
+    # print(f"{count_words(contents)} words found in the document")
+    # print(count_characters(contents))
+    character_count = count_characters(contents)
+    list = sort(character_count)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(contents)} total words")
+    print("--------- Character Count -------")
+    for item in list:
+        key = item["char"]
+        if key.isalpha():
+            print(f"{key}: {item["num"]}")
+    print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,17 @@ def count_characters(text):
         else:
             dict[lowercase] = 1
     return dict
+
+def sort(dict):
+    list_of_dicts = []
+    for key in dict:
+        current_dict = {}
+        current_dict["char"] = key
+        current_dict["num"] = dict[key]
+        list_of_dicts.append(current_dict)
+
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
+
+def sort_on(items):
+    return items["num"]
